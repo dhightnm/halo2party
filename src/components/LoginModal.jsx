a9f26ee5a0ba12b7e07f78771a560f4db4762c18
Devin Hight <devin@deliverr.com>
2023-11-23 00:35:13 -0700

    add duo login

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./modal.css";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDoh5GyUgPbcv8SIySb-x7KwSpDTIPU1l4",
@@ -16,41 +16,57 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const LoginModal = ({ show, onClose }) => {
-    if (!show) {
-      return null;
-    }
-  
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const auth = getAuth();
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-            .then((result) => {
-                // Handle the result
-            })
-            .catch((error) => {
-                // Handle errors
-            });
-    }
-  
-    return (
-        <div className="modal-backdrop">
-          <div className="modal-content">
-            <form onSubmit={handleLogin} className="login-form">
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" required />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" required />
-              </div>
-              <button type="submit" className="login-button">Login</button>
-              <button type="button" onClick={onClose} className="close-button">Close</button>
-            </form>
-          </div>
-        </div>
-      );
+  if (!show) {
+    return null;
+  }
+
+  const auth = getAuth();
+
+  const handleEmailPasswordLogin = (e) => {
+      e.preventDefault();
+      const email = e.target.email.value;
+      const password = e.target.password.value;
+      signInWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+              // Handle the successful authentication
+              onClose(); // Close the modal
+          })
+          .catch((error) => {
+              // Handle errors
+          });
   };
+
+  const handleGoogleLogin = () => {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth, provider)
+          .then((result) => {
+              // Handle the successful authentication
+              onClose(); // Close the modal
+          })
+          .catch((error) => {
+              // Handle errors
+          });
+  };
+  
+  return (
+    <div className="modal-backdrop">
+        <div className="modal-content">
+            <form onSubmit={handleEmailPasswordLogin} className="login-form">
+                <div className="form-group">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit" className="login-button">Login with Email</button>
+            </form>
+            <button onClick={handleGoogleLogin} className="login-button">Login with Google</button>
+            <button type="button" onClick={onClose} className="close-button">Close</button>
+        </div>
+    </div>
+);
+};
 
 export default LoginModal;
